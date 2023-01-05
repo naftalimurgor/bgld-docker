5a6415ce21c458431e27f1e9215430f6bc461d38
naftalimurgor <murgornaftali@gmail.com>
2023-01-05 19:21:25 +0300

    update `bootstrap.sh` script

--- bootstrap-host.sh
+++ bootstrap-host.sh
@@ -12,9 +12,8 @@ shift
 free -m
 
 # Always clean-up, but fail successfully
-docker kill bgld-node 2>/dev/null || true
-docker rm bgld-node 2>/dev/null || true
-stop docker-bgld 2>/dev/null || true
+docker kill bgld 2>/dev/null || true
+docker rm bgld 2>/dev/null || true
 
 # Always pull remote images to avoid caching issues
 if [ -z "${BGL_IMAGE##*/*}" ]; then
@@ -26,9 +25,9 @@ docker volume create --name=bgld-data
 docker run -v bgld-data:/BGL --rm $BGL_IMAGE bgl_init
 
 # Start bgld via upstart and docker
-curl https://raw.githubusercontent.com/naftalimurgor/docker-bgld/master/upstart.init > /etc/init/docker-BGL.conf
-start docker-bgld
+curl https://raw.githubusercontent.com/naftalimurgor/bgld-docker/master/upstart.init > /etc/init/docker-BGL.conf
+start bgld
 
 set +ex
 echo "Resulting bgl.conf:"
-docker run -v bgld-data:/BGL --rm $BTC_IMAGE cat /BGL/.BGL/BGL.conf
+
